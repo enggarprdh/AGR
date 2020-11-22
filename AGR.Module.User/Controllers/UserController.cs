901fb8993ab6d256774d8547f8f4b28c558e4fa6
R. Enggar Prathita Joharnovta <enggarprathitaj@gmail.com>
2020-11-23 09:18:11 +1100

    Se

---
+++ AGR.Module.User/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BasicAuthentication;
+using IdentityNET;
+
+namespace AGR.Module.User
+{
+    public class UserController : ApiController
+    {
+        [BasicAuthentication]
+        [HttpGet]
+        public IHttpActionResult Values()
+        {
+            var result = User.UserName();
+            return Ok(result);
+        }
+    }
+}
